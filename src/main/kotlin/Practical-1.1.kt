96ac771c8d3c228f63d706ba07a929093f58b294
student <student@gnu.ac.in>
2023-07-14 12:33:40 +0530

    hy

---
+++ src/main/kotlin/Practical-1.1.kt
@@ -0,0 +1,20 @@
+fun main() {
+    println("Welcome uvpce")
+    var a = 16;
+    var b = 2.3;
+    var c = 'T';
+    var d = "HMS";
+    var e = 96.36;
+    var f = 338927847;
+    var g = -2
+    var h = 127
+    println("integer value:$a")
+    println("float value:$b")
+    println("Character value:$c")
+    println("string value:$d")
+    println("Double value:$e")
+    println("long value:$f")
+    println("Short value:$g")
+    println("Byte value:$h")
+}
+
