234ddd27dcc1edadc6e2acde22b1f583b336e838
student <student@gnu.ac.in>
2023-07-21 11:45:02 +0530

    19/7

--- src/main/kotlin/Practical-1.2.kt
+++ src/main/kotlin/Practical-1.2.kt
@@ -1,3 +1,11 @@
 fun main(){
-    println("student data")
+    var num1:Int=34
+    var num2:Double=num1.toDouble()
+    println("$num2: Integer to double ")
+    var s1:String="58"
+    var num3:Double=s1.toDouble()
+    println("$num3: String to double ")
+    var s2:String="23"
+    var num4:Int=s2.toInt()
+    println("$num4: String to integer ")
 }
