03a0909f8acff86497ae97fe01264ac60cf83f6a
payal <payalbenmeda19@gnu.ac.in>
2023-10-15 10:57:46 +0530

    hy

--- src/main/kotlin/Practical-1.1.kt
+++ src/main/kotlin/Practical-1.1.kt
@@ -1,40 +1,21 @@
-fun main() {
-    var a: Int = 5
-    var b: Float = 1.5F
-    var c: Char = 'T'
-    var d: Double = 24.57
-    var e: Boolean = false
-    var f: String = "hello"
-    var g: Long = 333892456
-    var h: Short = -2
-    var i: Byte = 127
-
-    println("$a:Integer variable")
-    println("$b:Float variable")
-    println("$c:Character variable")
-    println("$d:Double variable")
-    println("$e:Boolean variable")
-    println("$f:String variable")
-    println("$g:Long variable")
-    println("$h:Short variable")
-    println("$i:Byte variable")
+fun main()
+{
+    var a = 22;
+    var f = 1.5;
+    var c = 'T';
+    var s = "HMS";
+    var g = false;
+    var d = 96.36;
+    var l = 338927847;
+    var h = -2;
+    var t = 127;
+    println("Integer Value: $a")
+    println("Float Value: $f")
+    println("Character Value: $c")
+    println("String Value: $s")
+    println("Boolean Value: $g")
+    println("Double Value: $d")
+    println("Long Value: $l")
+    println("Short Value: $h")
+    println("Byte Value: $t")
 }
-    //val j:Int=5
-    //val k:Double=22.3
-    //val l:Float=1.5F
-    //val m:Char='k'
-    //val n:Boolean=true
-    //val o:Long=256432467
-    //val p:Short=-2
-    //val q:Byte=123
-    //val r:String="Hii"
-
-    //println("$j:integer constant")
-    //println("$k:double constant")
-    //println("$l:float constant")
-    //println("$m:char constant")
-    //println("$n:boolean constant")
-    //println("$o:long constant")
-    //println("$p:short constant")
-    //println("$q:byte constant")
-    //println("$r:string constant")
